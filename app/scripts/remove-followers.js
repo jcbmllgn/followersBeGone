701bd20aa312e6490db5d8d3e3a18cfa5fcda43e
jcbmllgn <jacobdmulligan@gmail.com>
2012-12-14 14:22:56 -0500

    First commit

---
+++ app/scripts/remove-followers.js
@@ -0,0 +1,125 @@
+var i = 1,
+    count = 0,
+    num = 0,
+    rating_array = [],
+    followers = 0,
+    following = 0,
+    userFollowers = $('.profile-card').find("[data-nav='followers'] strong").text(),
+    userFollowers = parseInt( userFollowers.replace(/,/g, "") ),
+    rating = 0,
+    THEcounter=0,
+    $currItem;
+
+function checkRating(item_id) {
+  return rating_array[item_id][0];
+}
+
+function setRating(i, newClass) {
+  // alert('My Followers: ' + followers + '. Following: ' + following + '. Tweets: ' + tweets );
+
+  rating = parseInt(followers)/parseInt(following)*parseInt(tweets);
+
+  rating_array.push([rating,num,username]);
+
+  num++;
+}
+
+function checkVariable(i, newClass) {
+
+  var a = $('.profile-modal .profile-modal-header');
+  if (a) {
+    tweets      = $('#profile_popup').find("[data-nav='profile'] strong").text(),
+    tweets      = parseInt( tweets.replace(/,/g, "") ),
+    following   = $('#profile_popup').find("[data-nav='following'] strong").text(),
+    following   = parseInt( following.replace(/,/g, "") ),
+    followers   = $('#profile_popup').find("[data-nav='followers'] strong").text(),
+    followers   = parseInt( followers.replace(/,/g, "") ),
+    username    = $('#profile_popup').find("h2.fullname a").text();
+
+    if( newClass && following > 1 || followers > 1 ){
+      setRating(i, newClass);
+    } else {
+      setTimeout("checkVariable()" , 200); // call myself again in 50 msecs
+    }
+  }
+  else {
+      setTimeout("checkVariable()" , 200); // call myself again in 50 msecs
+      // alert('no loaded')
+  }
+};
+
+function myLoop () {
+   setTimeout(function () {
+    newClass = 'count' + i;
+    var $currItem = $('#stream-items-id div.stream-item:nth-child(' + i + ')'),
+        item_length = $('#stream-items-id div.stream-item').length;
+
+    $currItem
+      .find('strong.fullname').trigger('click')
+      .addClass(newClass);
+
+    tweets      = $('#profile_popup').find("[data-nav='profile'] strong").text(),
+    tweets      = parseInt( tweets.replace(/,/g, "") ),
+    following   = $('#profile_popup').find("[data-nav='following'] strong").text(),
+    following   = parseInt( following.replace(/,/g, "") ),
+    followers   = $('#profile_popup').find("[data-nav='followers'] strong").text()
+    followers   = parseInt( followers.replace(/,/g, "") );
+
+    setTimeout("checkVariable(i, newClass)" , 250);
+
+    $("html, body").animate({ scrollTop: $(document).height() }, "fast");
+
+    i++;
+
+    if ( 10 === i ) { // item_length
+      $("html, body").animate({ scrollTop: $(document).height() }, "fast", function(){
+        item_length = $('#stream-items-id div.stream-item').length;
+          if ( 10 === i ) {
+            alert('done. item_length: ' + item_length + ' and i: ' + i );
+          } else {
+            myLoop();
+          }
+      });
+    } else {
+      myLoop();
+    }
+
+   }, 550)
+}
+
+myLoop();
+
+function deleteFollower(limit) {
+
+  THEcounter = 0;
+
+  $.each(rating_array, function() {
+    rating = this[0];
+    itentifier = this[1]+1;
+
+    if (rating < limit) {
+      THEcounter++;
+      // $('#stream-items-id div.stream-item:nth-child(' + itentifier + ') .user-dropdown .dropdown-menu .block-text').trigger('click');
+
+      $('#stream-items-id div.stream-item:nth-child(' + itentifier + ')').css('background','blue');
+    }
+  });
+};
+
+deleteFollower(2.5);
+
+      // This will delete a user:
+      // $('#stream-items-id div.stream-item:nth-child(' + itentifier + ') .user-dropdown .dropdown-menu .block-text').trigger('click');
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/scripts/remover.js
@@ -0,0 +1,6 @@
+function makeDashboard() {
+  var $removedItems = $('.dashboard .enhanced-media-thumbnails, .dashboard [data-component-term="similar_user_recommendations"], .dashboard .site-footer')
+  $removedItems.fadeOut();
+
+  console.log('Remover Set');
+}
