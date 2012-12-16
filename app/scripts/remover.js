04627451f2b185f93507555351bdd3b30042c6de
jcbmllgn <jacobdmulligan@gmail.com>
2012-12-15 19:51:04 -0500

    ...Lot's o' progress, not going to comment ; '

--- app/scripts/remover.js
+++ app/scripts/remover.js
@@ -1,6 +1,7 @@
 // Settings:
 
-var $removedItems,
+var limit = 80,
+    $removedItems,
     i = 1,
     count = 0,
     num = 0,
@@ -37,15 +38,17 @@ function setRating(i, newClass) {
   num++;
 }
 
-function setRed(num, rating) {
+function setRed() {
 
   $.each(rating_array, function() {
     rating = this[0];
     itentifier = this[1]+1;
-    if (rating < 2.5) {
-      $currItem
+    var $item = $('#stream-items-id div.stream-item:nth-child(' + itentifier + ')');
+    if (rating < 2.5 && !$item.hasClass('setRed')) {
+      $item
         .css({'background-color' : '#f2dede' , 'border' : '1px solid #eed3d7' , 'position' : 'relative'})
-        .append('<button class="btn noblock" data-ratingID="' + num + '" style="position: absolute; top: 9px; right: -138px;"><span>Don\'t block me!</span></button>');
+        .addClass('setRed')
+        .append('<button class="btn noblock" data-ratingID="' + itentifier + '" style="position: absolute; top: 9px; right: -138px;"><span>Don\'t block me!</span></button>');
     }
   });
 
@@ -68,7 +71,7 @@ function deleteFollower(limit) {
 
   $.each(rating_array, function() {
     rating = this[0];
-    itentifier = this[1]+1;
+    itentifier = this[1] + 1;
 
     if (rating < limit) {
       THEcounter++;
@@ -81,6 +84,8 @@ function deleteFollower(limit) {
       $('.fake-count-down').text(THEcounter);
     }
   });
+  alert('Huzzah! You just deleted ' + THEcounter + ' fake followers!');
+  alert('You can now reload the page ')
 };
 
 function checkVariable(i, newClass) {
@@ -129,10 +134,10 @@ function followerScan (num_scan) {
 
     i++;
 
-    if ( 60 === i ) { // item_length
+    if ( 100 === i ) { // item_length
       $("html, body").animate({ scrollTop: $(document).height() }, "fast", function(){
         item_length = $('#stream-items-id div.stream-item').length;
-          if ( 60 === i ) {
+          if ( 100 === i ) {
             // alert('Scan is done, read the directions box for the next (and last) step.');
             deleteStep();
           } else {
@@ -151,7 +156,7 @@ function deleteStep() {
     $('.close-modal-background-target').trigger('click');
     $('.directions-block')
       .fadeOut().empty()
-      .append('<h2>Now to remove your fake follwers!</h2><br><p style="font-weight: bold;">You have <span class="fake-count">' + numFake + '</span> fake followers!</p><br><p>Scroll down the list on the right, you\'ll see that all fake followers are highlighted in red. If any of these followers where wrongly marked as fake, just click on the "Don\'t block me" button then move on to step two.</p><br><p><strong>Last step!</strong> Now that we\'ve selected the fake followers, click this button to remove them:</p><br><br><button class="btn" id="delete-followers">Delete fake followers</button><br> ').fadeIn(); // Inserts directions for next step.
+      .append('<h2>Now to remove your fake follwers!</h2><br><p style="font-weight: bold;">You have <span class="fake-count">' + numFake + '</span> fake followers!</p><br><p>Scroll down the list on the right, you\'ll see that all fake followers are highlighted in red. If any of these followers where wrongly marked as fake, just click on the "Don\'t block me" button then move on to step two.</p><br><p><strong>Last step!</strong> Now that we\'ve selected the fake followers, click this button to remove them:</p><br><br><button class="btn" id="delete-followers">Delete fake followers</button><br><p style="font-style: italic;">Depending on the number of followers you have, this may take a couple minutes to process!</p>').fadeIn(); // Inserts directions for next step.
 
     $('#delete-followers').click(function(){
     $('body').append('<div style="position: fixed;left: 0px;top: 0px;width: 100%;height: 100%;background-color: rgba(62, 62, 62, 0.6);" class="popover"></div><div style="z-index: 1000000;position: absolute;left: 30%;width: 400px;padding: 30px 10px;min-height: 175px;margin: 100px auto 0px;text-align: center;background-color: white;" class="wait-box"><h1>Removed <span id="fake-count-down">0</span> followers out of ' + numFake +'</h1></div>')
